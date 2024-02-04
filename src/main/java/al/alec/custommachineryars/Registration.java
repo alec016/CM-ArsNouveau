3dc5af0d6fa1a2bf2a9dcff104a8228db13b01c1
alec016 <78179790+alec016@users.noreply.github.com>
2024-02-04 02:07:57 +0100

    fix: data registration remove blockpos

--- src/main/java/al/alec/custommachineryars/Registration.java
+++ src/main/java/al/alec/custommachineryars/Registration.java
@@ -4,22 +4,15 @@ package al.alec.custommachineryars;
 import al.alec.custommachineryars.components.SourceMachineComponent;
 import al.alec.custommachineryars.components.variant.item.SourceItemComponentVariant;
 import al.alec.custommachineryars.guielement.SourceGuiElement;
-import al.alec.custommachineryars.network.data.BlockPosData;
-import al.alec.custommachineryars.network.syncable.BlockPosSyncable;
 import al.alec.custommachineryars.requirement.SourceRequirement;
 import al.alec.custommachineryars.requirement.SourceRequirementPerTick;
 import dev.architectury.registry.registries.Registries;
-import dev.architectury.registry.registries.RegistrySupplier;
-import fr.frinn.custommachinery.CustomMachinery;
 import fr.frinn.custommachinery.api.ICustomMachineryAPI;
 import fr.frinn.custommachinery.api.component.MachineComponentType;
 import fr.frinn.custommachinery.api.component.variant.RegisterComponentVariantEvent;
 import fr.frinn.custommachinery.api.guielement.GuiElementType;
 import fr.frinn.custommachinery.api.network.DataType;
 import fr.frinn.custommachinery.api.requirement.RequirementType;
-import fr.frinn.custommachinery.common.network.data.BooleanData;
-import fr.frinn.custommachinery.common.network.syncable.BooleanSyncable;
-import net.minecraft.core.BlockPos;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 
@@ -34,8 +27,6 @@ public abstract class Registration {
   public static final RegistryObject<MachineComponentType<SourceMachineComponent>> SOURCE_MACHINE_COMPONENT = MACHINE_COMPONENTS.register("source", () -> MachineComponentType.create(SourceMachineComponent.Template.CODEC));
   public static final RegistryObject<RequirementType<SourceRequirement>> SOURCE_REQUIREMENT = REQUIREMENTS.register("source", () -> RequirementType.world(SourceRequirement.CODEC));
   public static final RegistryObject<RequirementType<SourceRequirementPerTick>> SOURCE_REQUIREMENT_PER_TICK = REQUIREMENTS.register("source_per_tick", () -> RequirementType.world(SourceRequirementPerTick.CODEC));
-  public static final RegistryObject<DataType<BlockPosData, BlockPos>> BLOCKPOS_DATA = DATAS.register("blockpos", () -> DataType.create(BlockPos.class, BlockPosSyncable::create, BlockPosData::new));
-
 
   public static void registerComponentVariants(RegisterComponentVariantEvent event) {
     event.register(fr.frinn.custommachinery.common.init.Registration.ITEM_MACHINE_COMPONENT.get(), SourceItemComponentVariant.ID, SourceItemComponentVariant.CODEC);
